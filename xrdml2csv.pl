59a1190538abc58fd08966f9adc97cdea6b837b0
Ivan S. Titov <iohann.s.titov@gmail.com>
2011-05-26 15:11:33 +0200

    Fixed typo in xy conversion, actually leading to csv-like file

--- xrdml2csv.pl
+++ xrdml2csv.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl -w
-#use strict;
+use strict;
 use XML::Mini::Document;
-
+foreach  $_(@ARGV){
 my $xmlDoc = XML::Mini::Document->new();
-$xmlDoc->parse('CoTiSn-1.xrdml');
-my $part = $xmlDoc->getElement('comment');
-my $entry = $part->getElementByPath('entry',1);
-print '[Measurement conditions]', "\n" ;
+$xmlDoc->parse($_);
+s/\.xrdml$|$/\.csv/i ;
+open OUT, ">", "$_" ;
+select OUT;
+
+print "\[Measurement conditions\]\n" ;
 print "Sample identification,\n" ;
+my $part = $xmlDoc->getElement('comment');
 print 'Comment - 1,"', $part->getElementByPath('entry',1)->getValue, "\"\n" ; 
 print 'Comment - 2,"', $part->getElementByPath('entry',2)->getValue, "\"\n" ;
 print 'Comment - 3,"', $part->getElementByPath('entry',3)->getValue, "\"\n" ;
@@ -19,12 +22,12 @@ print "Anode material,", $part->getElementByPath('incidentBeamPath/xRayTube/anod
 print "K-Alpha1 wavelength,", $part->getElementByPath('usedWavelength/kAlpha1')->getValue, "\n" ;
 print "K-Alpha2 wavelength,", $part->getElementByPath('usedWavelength/kAlpha2')->getValue, "\n" ;
 print "Ratio K-Alpha2/K-Alpha1,", $part->getElementByPath('usedWavelength/ratioKAlpha2KAlpha1')->getValue, "\n" ;
-print "Monochromator used,NO\n" ;
+print "Monochromator used,NO\n" ; #Couldn't find corresponding field in .xrdml file :-(
 print "Generator voltage, ", $part->getElementByPath('incidentBeamPath/xRayTube/tension')->getValue, "\n" ;
 print "Tube current, ", $part->getElementByPath('incidentBeamPath/xRayTube/current')->getValue, "\n" ;
 print "File date and time, " , "\n" ;
-print "Unit cell," , "\n" ;
-print "h k l, 0 0 0 " , "\n" ;
+print "Unit cell," , "\n" ; #Couldn't find corresponding field in .xrdml file :-(
+print "h k l, 0 0 0 " , "\n" ; #Couldn't find corresponding field in .xrdml file :-(
 print "Scan axis,", $part->getElementByPath('scan/')->attribute('scanAxis'), "\n" ;
 print "Scan range,", $part->getElementByPath('scan/dataPoints/positions/startPosition',1,1,1,1)->getValue(),",",$part->getElementByPath('scan/dataPoints/positions/endPosition',1,1,1,1)->getValue(), "\n" ;
 $_ = $part->getElementByPath('/scan/dataPoints/intensities')->getValue();
@@ -32,13 +35,15 @@ s/\n//g;
 my @dpoints = split / /, $_ ;
 chomp @dpoints;
 my $step_size =  (( sprintf "%.12f",$part->getElementByPath('scan/dataPoints/positions/endPosition',1,1,1,1)->getValue() - sprintf "%.12f",$part->getElementByPath('scan/dataPoints/positions/startPosition',1,1,1,1)->getValue())/ (@dpoints - 1)) ;
-printf "Scan step size, %8.7f\n", $step_size , "\n" ;
+printf "Scan step size, %8.7f\n", $step_size ;
 printf "No. of points, %d\n" , scalar @dpoints ;
 print "Scan type," , $part->getElementByPath('scan')->attribute('mode'), "\n" ;
 print "Time per step," , $part->getElementByPath('scan/dataPoints/commonCountingTime')->getValue(), "\n" ;
-print '[Scan points]', "\n" ;
-print "Angle,Intensity" ;
+print "\[Scan points\]\n" ;
+print "Angle,Intensity\n" ;
 my $step = (sprintf "%.12f",$part->getElementByPath('scan/dataPoints/positions/startPosition',1,1,1,1)->getValue())-$step_size; 
-foreach $dpoints (@dpoints) {
-	printf "\n%.9f,%.1f", $step+=$step_size, $dpoints;
+foreach my $dpoints (@dpoints) {
+	printf  "%.9f,%.1f\n", $step+=$step_size, $dpoints;
+}
+close OUT;
 }
--- xrdml2xy.pl
+++ xrdml2xy.pl
@@ -16,7 +16,7 @@ chomp @dpoints;
 my $step_size =  (( sprintf "%.12f",$part->getElementByPath('scan/dataPoints/positions/endPosition',1,1,1,1)->getValue() - sprintf "%.12f",$part->getElementByPath('scan/dataPoints/positions/startPosition',1,1,1,1)->getValue())/ (@dpoints - 1)) ;
 my $step = (sprintf "%.12f",$part->getElementByPath('scan/dataPoints/positions/startPosition',1,1,1,1)->getValue())-$step_size; 
 foreach my $dpoints (@dpoints) {
-	printf  "%.9f,%.1f\n", $step+=$step_size, $dpoints;
+	printf  "%.9f %.1f\n", $step+=$step_size, $dpoints;
 }
 close OUT;
 }
