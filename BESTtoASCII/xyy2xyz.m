b8e6a23837a16662c097d4e15bb2997b33a3320c
Ivan S. Titov <iohann.s.titov@gmail.com>
2012-05-06 19:31:51 +0200

    Minor changes xyy2xyz.m

--- BESTtoASCII/xyy2xyz.m
+++ BESTtoASCII/xyy2xyz.m
@@ -5,7 +5,7 @@
 #     z_1  z_2  z_3  ...      x_1 y_1 z_1
 # x_1 y_11 y_12 y_13 ... ->   x_2 y_2 z_2
 # x_2 y_21 y_22 y_23 ...      x_3 y_3 z_3
-
+# Such data representation is required by Gnuplot and (sometimes) by Origin 7.x
 function [data_xyz] = xyy2xyz(data_xyy)
 x = size(data_xyy)(1)-1;
 z = size(data_xyy)(2)-1;
