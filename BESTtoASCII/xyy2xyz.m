edc3bd1e327eada1843aae219e9ec98eca85aaa1
Ivan S. Titov <iohann.s.titov@gmail.com>
2012-05-06 19:30:42 +0200

    Added xyy2xyz.m

---
+++ BESTtoASCII/xyy2xyz.m
@@ -0,0 +1,18 @@
+## Author: iohann.s.titov@gmail.com
+## Keywords: format conversion
+## Maintainer: iohann.s.titov
+#This function turns xyy table into xyz one.
+#     z_1  z_2  z_3  ...      x_1 y_1 z_1
+# x_1 y_11 y_12 y_13 ... ->   x_2 y_2 z_2
+# x_2 y_21 y_22 y_23 ...      x_3 y_3 z_3
+
+function [data_xyz] = xyy2xyz(data_xyy)
+x = size(data_xyy)(1)-1;
+z = size(data_xyy)(2)-1;
+data_xyz = zeros(x*z,3);
+for i = 0:z-1
+data_xyz(i*x+1:(i+1)*x,1)=data_xyy(2:end,1);
+data_xyz(i*x+1:(i+1)*x,2)=data_xyy(2:end,i+2);
+data_xyz(i*x+1:(i+1)*x,3)=data_xyy(1,i+2);
+endfor
+endfunction
